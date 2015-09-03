f1559051d86fbf58c0c516e0be988aff8ec2091e
Nipat <i3anky@gmail.com>
2015-09-03 15:03:19 +0700

    Create InputStream

--- app/src/main/java/app/checkraka/bank/myrestaurant/MainActivity.java
+++ app/src/main/java/app/checkraka/bank/myrestaurant/MainActivity.java
@@ -1,9 +1,21 @@
 package app.checkraka.bank.myrestaurant;
 
+import android.database.sqlite.SQLiteDatabase;
+import android.os.StrictMode;
+import android.support.v4.util.Pools;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,9 +32,63 @@ public class MainActivity extends AppCompatActivity {
         createAndConnectDatabase();
 
         //Tester add value
-        testerAddValue();
+//        testerAddValue();
+
+        //Delete All Data
+       //Synchronize JSON to SQLite
+        synJSONtoSQLite();
 
     }//On Create
+
+    private void synJSONtoSQLite() {
+        //Change Policy
+        StrictMode.ThreadPolicy objThreadPolicy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(objThreadPolicy);
+
+        int time = 0;
+        while (time <= 1)
+        {
+            InputStream objInputStream = null;
+            String strJSON = null;
+            String user_url = "http://swiftcodingthai.com/3sep/php_get_data_bank.php";
+            String food_url = "http://swiftcodingthai.com/3sep/php_get_data_food.php";
+
+            HttpPost objHttpPost = null;
+
+            //1. Create Input Stream
+            try {
+
+                HttpClient objHttpClient = new DefaultHttpClient();
+                if (time != 1) {
+                    objHttpPost = new HttpPost(user_url);
+                } else {
+                    objHttpPost = new HttpPost(food_url);
+                }
+
+                //Response
+                HttpResponse objHttpResponse = objHttpClient.execute(objHttpPost);
+                HttpEntity objHttpEntity = objHttpResponse.getEntity();
+                objInputStream = objHttpEntity.getContent();
+                
+            }
+            catch (Exception e)
+            {
+                Log.d("Rest","Input ==> "+e.toString());
+            }
+
+            //2. Create strJSON
+            //3. Update to SQLite
+
+            time +=1;
+        }
+    }//synJSONtoSQLite
+
+    private void deleteAllData() {
+        //All Delete
+        SQLiteDatabase objSqLiteDatabase = openOrCreateDatabase("Restaurant.db",MODE_PRIVATE,null);
+        objSqLiteDatabase.delete("userTABLE",null,null);
+        objSqLiteDatabase.delete("foodTABLE",null,null);
+    }
 
     private void testerAddValue() {
         objUserTABLE.addNewUser("admin","12345","Nipat");
